cde34a4035f9a50b432f1d5a1620bf37621828b3
tatarynrm <tatarynrm@gmail.com>
2025-05-14 11:46:32 +0300

    ewqeq

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,17 @@ import dotenv from "dotenv";
 import telegramAuthRouter from "./routes/auth.route";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import bot from "./bot/bot"; // Імпортуємо бота
+import bot from "./bot/bot"; // Telegram bot import
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-
+// Allowed origins for CORS
 const allowedOrigins = ['https://skulldate.site', 'https://www.skulldate.site'];
 
+// CORS configuration
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -22,18 +23,29 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true, // Allow credentials (cookies, tokens)
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.json()); // Для парсингу application/json
-app.use(bodyParser.urlencoded({ extended: true }));
 
+// Middleware
+app.use(express.json());  // For parsing application/json
+app.use(cookieParser());  // For parsing cookies
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+// Routes
 app.use("/auth", telegramAuthRouter);
 
+// Start the bot and server
+const startServer = async () => {
+  try {
+    await bot.launch(); // Launch the bot if needed
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting the bot:', error);
+  }
+};
 
-
-bot.launch()
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+startServer();
