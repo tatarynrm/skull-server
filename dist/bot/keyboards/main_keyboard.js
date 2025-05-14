77e5a39852d56adc0c095af7b7468aa6183b974c
tatarynrm <tatarynrm@gmail.com>
2025-05-14 12:34:50 +0300

    ewqqeweqw

--- dist/bot/keyboards/main_keyboard.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MainKeyboard = void 0;
-exports.MainKeyboard = {
-    keyboard: [[{ text: "Хлопець" }, { text: "Дівчина" }]],
-    resize_keyboard: true,
-};
