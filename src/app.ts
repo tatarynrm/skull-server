ef439da876a7410eaedac005313281dd4dc2a441
tatarynrm <tatarynrm@gmail.com>
2025-05-14 20:53:19 +0300

    ewqewq

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.json({ limit: '50mb' })); // Максимальний розмір тіла 50 MB
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Routes
 app.use("/auth", telegramAuthRouter);
 app.use("/cloudinary", cloudinaryRouter);
