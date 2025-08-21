7a8a18fd6ad82031bc2df4c3579986ebbcf040a9
tatarynrm <tatarynrm@gmail.com>
2025-08-22 02:59:47 +0300

    ewqewq

--- src/bot/cron-jobes/index.ts
+++ src/bot/cron-jobes/index.ts
@@ -1,6 +1,6 @@
-import { scheduleDeleteOldLikes } from "./likes.cron";
+import { LikesSchedule } from "./likes.cron";
 
 export const startAllCronJobs = async () => {
   // Видалення усіх лайків за минулий день в 00:05 ночі кожного дня
-  scheduleDeleteOldLikes();
+  LikesSchedule();
 };
